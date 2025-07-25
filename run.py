b496415f972050d54f50d237db1b32764ad8fd0c
SilentPwner <anmarkbrasly@gmail.com>
2025-07-25 18:14:27 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+
+def run_shell_script(script_path):
+    try:
+        # تشغيل السكربت مع توجيه المخرجات إلى stdout و stderr
+        result = subprocess.run(['bash', script_path], check=True)
+        print(f"Script {script_path} executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running script: {e}", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    script_path = './setup.sh'  # مسار السكربت
+    run_shell_script(script_path)
